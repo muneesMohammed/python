29a3610c35ab44d2b486afeffbe71f7fa2da39bf
muneesMohammed <marhaba.munees@gmail.com>
2022-08-08 14:37:55 +0530

    odd code

---
+++ odd.py
@@ -0,0 +1,7 @@
+lowerLimit=int(input("enter the lower limit"))
+upperLimit=int(input("enter the upper limit"))
+while lowerLimit<=upperLimit:
+    if lowerLimit%2!=0:
+        print(lowerLimit)
+    lowerLimit+=1
+ 
